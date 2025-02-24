1ead903f527fa75b6c8607a1e3d2e642f7b5cb18
Publio Melo <publiomelo@gmail.com>
2025-02-24 00:34:31 -0300

    Refactor puppeteer import/export to CommonJS syntax

--- scraping-final.js
+++ scraping-final.js
@@ -1,11 +1,11 @@
-import { use, launch } from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-use(StealthPlugin());
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const scrapeFlights = async ({ origin, destination, departureDate }) => {
-  const browser = await launch({
+  const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: null,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -128,4 +128,4 @@ const scrapeFlights = async ({ origin, destination, departureDate }) => {
   }
 };
 
-export default { scrapeFlights };
+module.exports = { scrapeFlights };
